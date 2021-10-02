5db2c191c913e111a41e433d3fba69a875f71a68
Ben Fernandes <dev.benfernandes@gmail.com>
2021-10-03 00:40:09 +0100

    Created initial migration and updated model

--- Models/Station.cs
+++ Models/Station.cs
@@ -1,8 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Tube.Models
 {
     public class Station
     {
+        [Key]
         public int Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
         public string Name { get; set; }
     }
 }
